45b1f7f098e6335b93b5b8376d7262213a9292dd
WowSoLaggy <dohxehapo@gmail.com>
2019-07-21 23:56:55 +0300

    (~) Marked util functions static

--- LaggySdk/FileSystemUtils.h
+++ LaggySdk/FileSystemUtils.h
@@ -6,13 +6,13 @@
 
 namespace Sdk
 {
-  std::string getParentFolder(const std::string& i_fileName)
+  static std::string getParentFolder(const std::string& i_fileName)
   {
     using namespace std::experimental::filesystem;
     return path(i_fileName).parent_path().string();
   }
 
-  std::string getExtension(const std::string& i_fileName)
+  static std::string getExtension(const std::string& i_fileName)
   {
     using namespace std::experimental::filesystem;
     return path(i_fileName).extension().string();
