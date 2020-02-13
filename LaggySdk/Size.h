7c8f3a3829951ebbeffcab72656bed972d0cd0d8
WowSoLaggy <dohxehapo@gmail.com>
2020-02-13 23:22:05 +0300

    (+) Added 'Size'

---
+++ LaggySdk/Size.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Vector.h"
+
+
+namespace Sdk
+{
+  using Size2I = Vector2I;
+  using Size3I = Vector3I;
+  using Size4I = Vector4I;
+  using Size2F = Vector2F;
+  using Size3F = Vector3F;
+  using Size4F = Vector4F;
+  using Size2D = Vector2D;
+  using Size3D = Vector3D;
+  using Size4D = Vector4D;
+
+} // ns Sdk
