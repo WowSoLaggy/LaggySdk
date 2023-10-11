8ecf0e7f5fe0f69f75b8bccfaa7fe20dd3457bb9
Anton Egorov <dohxehapo@gmail.com>
2023-10-11 22:30:10 +0300

    (+) Add location to all exceptions

--- LaggySdk/Contracts.h
+++ LaggySdk/Contracts.h
@@ -3,49 +3,64 @@
 #include <stdexcept>
 
 
+#define S1(x) #x
+#define S2(x) S1(x)
+#define LOCATION __FILE__ " : " S2(__LINE__)
+
+
 namespace Sdk
 {
-  class AssertFailedException : public std::runtime_error
+  class DxException : public std::runtime_error
   {
   public:
-    AssertFailedException()
-      : std::runtime_error("Assert failed")
+    DxException(const std::string& i_text)
+      : std::runtime_error(i_text)
     {
     }
   };
 
-  class DereferenceFailedException : public std::runtime_error
+
+  class AssertFailedException : public DxException
   {
   public:
-    DereferenceFailedException()
-      : std::runtime_error("Dereference failed")
+    AssertFailedException(const std::string& i_location)
+      : DxException("Assert failed at " + i_location)
     {
     }
   };
 
-  class PreconditionFailedException : public std::runtime_error
+  class DereferenceFailedException : public DxException
   {
   public:
-    PreconditionFailedException()
-      : std::runtime_error("Precondition failed")
+    DereferenceFailedException(const std::string& i_location)
+      : DxException("Dereference failed at " + i_location)
     {
     }
   };
 
-  class PostconditionFailedException : public std::runtime_error
+  class PreconditionFailedException : public DxException
   {
   public:
-    PostconditionFailedException()
-      : std::runtime_error("Postcondition failed")
+    PreconditionFailedException(const std::string& i_location)
+      : DxException("Precondition failed at " + i_location)
     {
     }
   };
 
-  class DeadEndException : public std::runtime_error
+  class PostconditionFailedException : public DxException
   {
   public:
-    DeadEndException()
-      : std::runtime_error("Dead end is reached")
+    PostconditionFailedException(const std::string& i_location)
+      : DxException("Postcondition failed at " + i_location)
+    {
+    }
+  };
+
+  class DeadEndException : public DxException
+  {
+  public:
+    DeadEndException(const std::string& i_location)
+      : DxException("Dead end is reached at " + i_location)
     {
     }
   };
@@ -54,20 +69,20 @@ namespace Sdk
 
 
 #define CONTRACT_ASSERT(condition) do { \
-  if (!(condition)) throw Sdk::AssertFailedException(); \
+  if (!(condition)) throw Sdk::AssertFailedException(LOCATION); \
 } while (false);
 
 #define CONTRACT_EXPECT(condition) do { \
-  if (!(condition)) throw Sdk::PreconditionFailedException(); \
+  if (!(condition)) throw Sdk::PreconditionFailedException(LOCATION); \
 } while (false);
 
 #define CONTRACT_ENSURE(condition) do { \
-  if (!(condition)) throw Sdk::PostconditionFailedException(); \
+  if (!(condition)) throw Sdk::PostconditionFailedException(LOCATION); \
 } while (false);
 
 #define CONTRACT_THROW do { \
-  throw Sdk::DeadEndException(); \
+  throw Sdk::DeadEndException(LOCATION); \
 } while (false);
 
 
-#define SAFE_DEREF(x) [&]() -> auto& { if (x) return *(x); else throw Sdk::DereferenceFailedException(); }()
+#define SAFE_DEREF(x) [&]() -> auto& { if (x) return *(x); else throw Sdk::DereferenceFailedException(LOCATION); }()
